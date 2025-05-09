c3f8d65b9e389853e808970c30edbc55ec4790cc
junhyeonpark <j0303p@gmail.com>
2025-05-09 12:28:14 +0900

    feat: 초기 level-1  컨트롤러

---
+++ src/nba/controller/depth/level/InitController.java
@@ -0,0 +1,29 @@
+package nba.controller.depth.level;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import nba.controller.CustomController;
+import nba.util.ScanUtil;
+import nba.util.View;
+
+@RequiredArgsConstructor
+@Getter
+public class InitController implements CustomController {
+
+    private final String name;
+
+    @Override
+    public String handle() {
+        View.printInitWithControl();
+        int next= ScanUtil.readInt();
+        switch (next){
+            case 1:
+                return "login";
+            case 2:
+                return "register";
+            default:
+                View.printValidNum();
+                return "init";
+        }
+    }
+}
