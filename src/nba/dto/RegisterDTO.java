9882fce85c359f070cb900ab0fdb4fb9876589c6
junhyeonpark <j0303p@gmail.com>
2025-05-12 10:14:51 +0900

    feat: 로그인,가입 dto

---
+++ src/nba/dto/RegisterDTO.java
@@ -0,0 +1,15 @@
+package nba.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterDTO {
+    private String email;
+    private String password;
+}
