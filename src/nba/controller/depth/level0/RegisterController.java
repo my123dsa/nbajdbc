14fbf8826c66e37f9a69522b38fdffbc2490d9ce
junhyeonpark <j0303p@gmail.com>
2025-05-12 10:13:52 +0900

    feat: 회원가입 컨트롤러

--- src/nba/controller/depth/level0/RegisterController.java
+++ src/nba/controller/depth/level0/RegisterController.java
@@ -3,20 +3,24 @@ package nba.controller.depth.level0;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import nba.controller.CustomController;
+import nba.service.LoginService;
 import nba.util.ScanUtil;
 import nba.util.View;
 
 @RequiredArgsConstructor
-@Getter
 public class RegisterController implements CustomController {
-
+    @Getter
     private final String name;
-
+    private final LoginService loginService;
     @Override
     public String handle() {
         View.printRegister();
         String next= ScanUtil.readLine();
         //todo  등록 프로세스
+        boolean result =loginService.register(next);
+        if(!result) {
+            return "register";
+        }
         return "login";
     }
 }
