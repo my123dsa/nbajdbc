ff5c45231d491451097f4d095a25e3dbc7632754
junhyeonpark <j0303p@gmail.com>
2025-05-12 10:13:42 +0900

    feat: 로그인 컨트롤러

--- src/nba/controller/depth/level0/LoginController.java
+++ src/nba/controller/depth/level0/LoginController.java
@@ -3,29 +3,33 @@ package nba.controller.depth.level0;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import nba.controller.CustomController;
+import nba.controller.MainController;
+import nba.dto.OwnerState;
+import nba.service.LoginService;
 import nba.util.ScanUtil;
 import nba.util.View;
 
 @RequiredArgsConstructor
-@Getter
 public class LoginController implements CustomController {
-
+    @Getter
     private final String name;
+    private final LoginService loginService;
 
     @Override
     public String handle() {
         View.printLogin();
-        String next= ScanUtil.readLine();
-        if(next.equals("register")){
+        String next = ScanUtil.readLine();
+        if (next.equals("register")) {
             return "register";
         }
-        //todo 로그인 확인
-        boolean result = false;
-        if(!result){
+
+        OwnerState ownerState = loginService.findByIdAndPassword(next);
+        if (ownerState == null) {
             return "login";
         }
-        else{
-            return "enter";
-        }
+
+        MainController.setOwnerState(ownerState);
+
+        return "enter";
     }
 }
