1d87761d153bf7c4f70a91a82e7c1e4340e96606
junhyeonpark <j0303p@gmail.com>
2025-05-12 10:28:39 +0900

    refactor: 가변인자 리팩

--- src/nba/controller/MainController.java
+++ src/nba/controller/MainController.java
@@ -3,13 +3,7 @@ package nba.controller;
 
 import lombok.Getter;
 import lombok.Setter;
-import nba.controller.depth.level.InitController;
-import nba.controller.depth.level0.LoginController;
-import nba.controller.depth.level0.RegisterController;
-import nba.controller.depth.level1.*;
-import nba.controller.depth.level2.*;
-import nba.controller.depth.level3.*;
-import nba.domain.Owner;
+import nba.dto.OwnerState;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,30 +12,15 @@ import java.util.Map;
 public class MainController {
     @Getter
     @Setter
-    private static Owner owner;
-    private final Map<String, CustomController> controllerMap = new HashMap<>();
+    private static OwnerState ownerState;
+    private final Map<String, CustomController> controllerMap;
 
-    public MainController(
-                          InitController initController,
-                          LoginController loginController,
-                          RegisterController registerController,
-                          EnterController enterController,
-                          TeamController teamController,
-                          PlayerEventController playerEventController,
-                          PlayerController playerController,
-                          ManagementController managementController,
-                          GameController gameController) {
-
-        // 모든 컨트롤러를 map에 등록
-        controllerMap.put(initController.getName(), initController);
-        controllerMap.put(loginController.getName(), loginController);
-        controllerMap.put(registerController.getName(), registerController);
-        controllerMap.put(enterController.getName(), enterController);
-        controllerMap.put(teamController.getName(), teamController);
-        controllerMap.put(playerEventController.getName(), playerEventController);
-        controllerMap.put(playerController.getName(), playerController);
-        controllerMap.put(managementController.getName(), managementController);
-        controllerMap.put(gameController.getName(), gameController);
+    public MainController(CustomController... controllers) {
+        controllerMap=new HashMap<>();
+    // 모든 컨트롤러를 map에 등록
+        for (CustomController controller : controllers) {
+            controllerMap.put(controller.getName(), controller);
+        }
     }
 
     public void play(String screen) {
