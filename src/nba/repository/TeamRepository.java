8198193173aa76345053c894b53db13d3780ec31
junhyeonpark <j0303p@gmail.com>
2025-05-12 12:29:12 +0900

    feat: 팀의 선수들 조회 로직 생성

--- src/nba/repository/TeamRepository.java
+++ src/nba/repository/TeamRepository.java
@@ -1,13 +1,79 @@
 package nba.repository;
 
 import lombok.RequiredArgsConstructor;
+import nba.dto.PlayerWithStats;
+import nba.dto.TeamWithPlayersAndStatsDTO;
 import nba.util.ConnectionFactory;
 
 import javax.sql.DataSource;
+import java.nio.channels.ScatteringByteChannel;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RequiredArgsConstructor
 public class TeamRepository {
     private final DataSource dataSource;
     private final PlayerRepository playerRepository;
+
+    public TeamWithPlayersAndStatsDTO findTeamWithPlayersById(int teamId) {
+        String sql = "SELECT " +
+                "t.id as team_id, t.name as team_name, t.money, t.wins, t.total_games, " +
+                "p.id as player_id, p.name as player_name, p.birth, p.salary, p.height, p.wing_span, p.position, p.IS_FA, " +
+                "s.shoot, s.pass, s.dribble, s.rebound, s.block, s.steal, s.point3 " +
+                "FROM team t " +
+                "JOIN player p ON p.TEAM_ID = t.ID " +
+                "JOIN stats s ON s.PLAYER_ID = p.ID " +
+                "WHERE t.ID = ?";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, teamId);
+            ResultSet rs = ps.executeQuery();
+
+            TeamWithPlayersAndStatsDTO team = null;
+            List<PlayerWithStats> players = new ArrayList<>();
+
+            while (rs.next()) {
+                if(team == null) {
+                    System.out.println("팀 있습니다");
+                    team = TeamWithPlayersAndStatsDTO.builder()
+                            .id(rs.getInt("team_id"))
+                            .name(rs.getString("team_name"))
+                            .money(rs.getLong("money"))
+                            .wins(rs.getInt("wins"))
+                            .totalGames(rs.getInt("total_games"))
+                            .players(players)
+                            .build();
+                }
+
+                PlayerWithStats player=  PlayerWithStats.builder()
+                        .id(rs.getInt("player_id"))
+                        .name(rs.getString("player_name"))
+                        .birth(rs.getDate("birth").toLocalDate())
+                        .salary(rs.getLong("salary"))
+                        .height(rs.getInt("height"))
+                        .wingSpan(rs.getInt("wing_span"))
+                        .position(rs.getString("position"))
+                        .isFA(rs.getBoolean("IS_FA"))
+                        .shoot(rs.getInt("shoot"))
+                        .pass(rs.getInt("pass"))
+                        .dribble(rs.getInt("dribble"))
+                        .rebound(rs.getInt("rebound"))
+                        .block(rs.getInt("block"))
+                        .steal(rs.getInt("steal"))
+                        .point3(rs.getInt("point3"))
+                        .build();
+                players.add(player);
+            }
+
+            return team;
+
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/nba/service/TeamService.java
+++ src/nba/service/TeamService.java
@@ -2,9 +2,14 @@ package nba.service;
 
 
 import lombok.RequiredArgsConstructor;
+import nba.dto.TeamWithPlayersAndStatsDTO;
 import nba.repository.TeamRepository;
 
 @RequiredArgsConstructor
 public class TeamService {
     private final TeamRepository teamRepository;
+
+    public TeamWithPlayersAndStatsDTO findTeamWithPlayersById(int teamId) {
+        return teamRepository.findTeamWithPlayersById(teamId);
+    }
 }
